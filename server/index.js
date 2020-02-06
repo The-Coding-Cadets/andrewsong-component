2a45bb6247eeac597184ab8a5326121a8f8043e8
Andrew Song <andrew.song.1122@gmail.com>
2020-02-06 10:04:44 -0800

    changed css selectors to avoid conflicts

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('../db');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3010;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
