3f615b48372b69a4598ce03417539e7e87d4e288
Andrew Song <andrew.song.1122@gmail.com>
2020-03-07 11:41:04 -0800

    webpack production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
-module.exports = {
-  entry: __dirname + '/src/Options.jsx',
-  module: {
-    rules: [
-      {
-        test: [/\.jsx$/],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
+const TerserPlugin = require('terser-webpack-plugin');
+
+  module.exports = {
+    mode: 'production',
+    optimization: {
+      minimizer: [new TerserPlugin({ /* additional options here */ })],
+    },
+    entry: __dirname + '/src/Options.jsx',
+    module: {
+      rules: [
+        {
+          test: [/\.jsx$/],
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
           }
         }
-      }
-    ]
-  },
-   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/client'
+      ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/client'
   }
 };
